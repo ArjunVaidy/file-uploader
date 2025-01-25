ace290edd28f559f355a07ce7e6cd53f83c6a203
Arjun <arjun071194@gmail.com>
2025-01-25 16:32:00 +0530

    pause receiving the data in socket from client until the file is created or opened.

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ server.on("connection", (socket) => {
   console.log("New Connection");
   socket.on("data", async (data) => {
     if (!fileHandle) {
+      socket.pause(); // don't receive the data until fiel is created
       fileHandle = await fs.open(`storage/test.txt`, "w");
       fileWriteStream = fileHandle.createWriteStream();
       fileWriteStream.write(data);
+
+      socket.resume(); // resume receiving the data
     } else {
       fileWriteStream.write(data);
     }
